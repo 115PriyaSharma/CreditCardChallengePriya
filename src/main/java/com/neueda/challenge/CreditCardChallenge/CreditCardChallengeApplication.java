e343c9c43cace61cb02438f73ba0be035d0cc395
dushant panchbhai <dushantrpanchbhai@gmail.com>
2023-08-09 10:08:20 +0000

    firstcommit

---
+++ src/main/java/com/neueda/challenge/CreditCardChallenge/CreditCardChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.neueda.challenge.CreditCardChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardChallengeApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardChallengeApplication.class, args);
+		System.out.println("hello!!!");
+	}
+
+}
---
+++ src/test/java/com/neueda/challenge/CreditCardChallenge/CreditCardChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neueda.challenge.CreditCardChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
